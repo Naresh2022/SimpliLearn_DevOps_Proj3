82dfafaaff59047fed4986150677bd35437c217b
Naresh Somepalli <99550247+Naresh2022@users.noreply.github.com>
2023-03-21 17:34:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 resource "aws_instance" "ansible_instance" {
     ami = "${var.ami_id}"
     count = "${var.number_of_instances}"
-    subnet_id = "${var.subnet_id}"
+#    subnet_id = "${var.subnet_id}"
     instance_type = "${var.instance_type}"
     key_name = "${var.ami_key_pair_name}"
 } 
