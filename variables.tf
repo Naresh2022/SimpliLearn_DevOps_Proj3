62c66a5974bbff54d7d70ff88b8b15ccfa99d0a6
Naresh Somepalli <99550247+Naresh2022@users.noreply.github.com>
2023-03-21 17:24:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "subnet_id" {
 
 variable "ami_id" {
         description = "The AMI to use"
-        default = "ami-0dcc1e21636832c5d"
+        default = "ami-830c94e3"
 }
 
 variable "number_of_instances" {
