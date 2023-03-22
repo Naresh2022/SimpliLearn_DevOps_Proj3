35ee1c795dddc25f1f8369ddbf45142ca2038bae
Naresh Somepalli <99550247+Naresh2022@users.noreply.github.com>
2023-03-22 11:02:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,4 +15,22 @@ resource "aws_instance" "ansible_instance" {
     count = "${var.number_of_instances}"
     instance_type = "${var.instance_type}"
     key_name = "${var.ami_key_pair_name}"
-} 
+  
+  provisioner "remote-exec" {
+    inline = [
+    "sudo amazon-linux-extras install ansible2 -y",
+    "sudo yum install git -y",
+    "git clone https://github.com/devops-school/ansible-hello-world-role /tmp/ans_ws",
+    "ansible-playbook /tmp/ans_ws/site.yaml"
+    ]
+  
+    connection {
+        # default user for ububtu ami from aws
+        # same key as in key_name above
+        type           = "ssh" 
+        user          = "ubuntu"
+        private_key   = "${file(var.private_key_path)}"
+        host          = "${self.public_ip}"
+    }
+  } 
+}
