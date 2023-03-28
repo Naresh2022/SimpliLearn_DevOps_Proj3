97c2cfe0925e2f2b5ee2ff5b9c5391ba47ac1eae
Naresh Somepalli <99550247+Naresh2022@users.noreply.github.com>
2023-03-28 13:19:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
+ ## access_key and Secret_key not required because since we are already inside the Account.
   #access_key = "${var.aws_access_key}"
   #secret_key = "${var.aws_secret_key}"
 }
@@ -21,8 +22,11 @@ resource "aws_instance" "ansible_instance" {
    
   provisioner "remote-exec" {
     inline = [
-    "sudo amazon-linux-extras install ansible2 -y",
-    "sudo yum install git -y",
+    "sudo apt update",
+    "sudo apt install software-properties-common",
+    "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+    "sudo apt install ansible",
+    "sudo apt install git",
     "git clone https://github.com/Naresh2022/SimpliLearn_DevOps_Proj3",
     "ansible-playbook install_jenkins_java_python.yaml"
     ]
@@ -31,10 +35,12 @@ resource "aws_instance" "ansible_instance" {
         # default user for ububtu ami from aws
         # same key as in key_name above
         type           = "ssh" 
-        #user          = "ec2-user"
-        user          = "ubuntu"
-        private_key   = "${file(var.private_key_path)}"
-        host          = "${self.public_ip}"
+        #user          = "ec2-user"  #for Amazon AMI default user is ec2-user
+        #user          = "ubuntu"   # for ubuntu is the default user for Ubuntu OS
+        user      = "root"
+        password    = var.root_password
+        #private_key   = "${file(var.private_key_path)}"
+        host          = self.public_ip
     }
   } 
 }
